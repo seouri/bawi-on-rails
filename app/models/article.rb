bb34efbcca265b15aa3c859cb3bbe524c15a124b
Joon Lee <seouri@gmail.com>
2010-12-15 05:59:23 -0500

    add uniqueness validation

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,6 @@ class Article < ActiveRecord::Base
   belongs_to :board, :counter_cache => true
   has_many :comments
   belongs_to :owner, :class_name => "User"
+
+  validates_uniqueness_of :article_no, :scope => :board_id
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,6 @@
 class Comment < ActiveRecord::Base
   belongs_to :article, :counter_cache => true
   belongs_to :owner, :class_name => "User"
+
+  validates_uniqueness_of :comment_no, :scope => :board_id
 end
