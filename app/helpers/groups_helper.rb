28f621d294233f2b03bf6feb61688ddc6bfc8b5d
Joon Lee <seouri@gmail.com>
2010-12-16 01:50:59 -0500

    refactored groups

--- app/helpers/groups_helper.rb
+++ app/helpers/groups_helper.rb
@@ -1,2 +1,9 @@
 module GroupsHelper
+  def groups(groups)
+    li = []
+    groups.each do |group|
+      li.push(content_tag(:li, link_to(group.name, group), :id => dom_id(group), :class => dom_class(group)))
+    end
+    content_tag(:ul, li.join("\n").html_safe, :id => "groups")
+  end
 end
