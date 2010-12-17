7ab916475b396abe4056293bb57ddc6b659dae34
Joon Lee <seouri@gmail.com>
2010-12-17 13:10:34 -0500

    show boards in a group

--- app/helpers/boards_helper.rb
+++ app/helpers/boards_helper.rb
@@ -1,2 +1,9 @@
 module BoardsHelper
+  def boards(boards)
+    li = []
+    boards.each do |board|
+      li.push(content_tag(:li, link_to(board.name, board), :id => dom_id(board), :class => dom_class(board)))
+    end
+    content_tag(:ul, li.join("\n").html_safe, :id => "boards")
+  end
 end
