539489a6e6b213eda94ba53a42c8f557ed99550d
Joon Lee <seouri@gmail.com>
2010-12-18 04:05:56 -0500

    show article_no

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -2,7 +2,8 @@ module ArticlesHelper
   def articles(articles)
     li = []
     articles.each do |article|
-      li.push(content_tag(:li, link_to(article.title, board_article_path(@board, article)), :id => dom_id(article), :class => dom_class(article)))
+      article_no = link_to(article.article_no, board_article_path(@board, article), :class => "article_no")
+      li.push(content_tag(:li, article_no + link_to(article.title, board_article_path(@board, article), :class => "title"), :id => dom_id(article), :class => dom_class(article)))
     end
     content_tag(:ul, li.join("\n").html_safe, :id => "articles")
   end
